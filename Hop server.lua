307fd3eb73148879a982986a76ab47a2ea7d86d3
sea <90987597+SEA40020Wachirawich@users.noreply.github.com>
2021-11-24 14:36:51 +0700

    Update Hop server.lua

--- Hop server.lua
+++ Hop server.lua
@@ -1,4 +1,4 @@
-local PlaceID = game.PlaceId
+--[[local PlaceID = game.PlaceId
 local AllIDs = {}
 local foundAnything = ""
 local actualHour = os.date("!*t").hour
@@ -58,4 +58,45 @@ function Teleport()
         end)
     end
 end
-Teleport()
+Teleport()]]
+writefile('servers.sss', game:service'HttpService':JSONEncode({}))
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+local HttpService, TPService = game:GetService"HttpService", game:GetService"TeleportService";
+local OtherServers = HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Asc&limit=100"))
+function joinNew()
+    if not isfile('servers.sss') then 
+        writefile('servers.sss',HttpService:JSONEncode({}))
+    end
+    local dontJoin = readfile('servers.sss') 
+    dontJoin = HttpService:JSONDecode(dontJoin)
+
+    for Index, Server in next, OtherServers["data"] do
+        if Server ~= game.JobId then
+            local j = true
+            for a,c in pairs(dontJoin) do 
+               if c == Server.id then 
+                   j = false 
+               end
+            end
+            if j then
+                table.insert(dontJoin,Server["id"])
+                writefile("servers.sss",HttpService:JSONEncode(dontJoin))
+                wait()
+                return Server['id']
+                
+                
+            end
+        end
+    end
+end
+while wait(5) do
+warn("finding the server")
+local server = joinNew()
+if not server then 
+    writefile("servers.sss",HttpService:JSONEncode({}))
+    local server = joinNew()
+    TPService:TeleportToPlaceInstance(game.PlaceId, server)
+else
+TPService:TeleportToPlaceInstance(game.PlaceId, server)
+end
+end
